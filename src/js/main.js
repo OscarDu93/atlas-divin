ecdb9cfe507994b26feefb2e0809edc1fcca4fd6
OscarDu93 <oscar.tomasetti@studentfr.ch>
2025-09-21 17:13:05 +0200

    dark/litght

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,16 @@ var toggleMenu = function toggleMenu() {
 };
 
 burger.addEventListener("click", toggleMenu);
+
+document.querySelector("html").setAttribute("data-theme", "light");
+
+const toggleDarkModeBtn = document.querySelector("#dark-mode-btn");
+const toggleLightModeBtn = document.querySelector("#light-mode-btn");
+
+toggleDarkModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "dark");
+});
+
+toggleLightModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "light");
+});
